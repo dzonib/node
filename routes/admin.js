c66f6a7e4d2f501a5827ccac59eee1f634746bcf
dzonib <nikolabosnjak381@gmail.com>
2018-11-11 16:08:59 +0100

    added styles to view started working with dynamic data

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+
+const router = express.Router()
+
+const rootDir = require('../utils/path')
+
+const products = []
+
+router.get('/add-product', (req, res, next) => {
+	res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+})
+
+router.post('/add-product', (req, res, next) => {
+	products.push({title: req.body.title})
+	console.log(products)
+	res.redirect('/')
+})
+
+exports.routes = router
+exports.products = products
---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+
+const rootDir = require('../utils/path')
+const adminData = require('./admin')
+
+
+
+const router = express.Router()
+
+router.get( '/', (req, res, next) => {
+    console.log(adminData)
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+})
+
+
+module.exports = router
