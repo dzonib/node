13a2b635a04aa064448e7bb3bc6ce3f249b18363
dzonib <nikolabosnjak381@gmail.com>
2018-11-13 07:23:00 +0100

    added 2 pages with some dynamic content

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,23 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
 
-const router = express.Router()
+const express = require('express');
 
-const rootDir = require('../utils/path')
+const rootDir = require('../util/path');
 
-const products = []
+const router = express.Router();
 
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-})
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+});
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-	products.push({title: req.body.title})
-	console.log(products)
-	res.redirect('/')
-})
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-exports.routes = router
-exports.products = products
+exports.routes = router;
+exports.products = products;
